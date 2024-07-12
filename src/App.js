fd453129b296e18768ba7fe1a8ad802038bd2103
rajateku <rajatekuiitb@gmail.com>
2024-07-12 10:01:38 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useRoutes,
+  Navigate,
+  Redirect,
+} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./components/Auth/Auth";
+import Signin from "./components/Auth/Signin";
+import Signup from "./components/Auth/Signup";
+import TermsAndConditions from "./components/Auth/TermsAndConditions";
+import Website from "./pages/website";
+import Home from "./pages/home";
+import Dashboard from "./pages/dashboard";
+import Jobs from "./pages/jobs";
+import Profile from "./pages/profile";
+import CompanyPage from "./pages/company";
+import InternshipPage from "./pages/internship";
+import SmoothScroll from "smooth-scroll";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./pages/ErrorFallback";
+import { getCurrentUser } from "./components/Helper";
+import ProtectedRoutesComponent from "./components/commonComponents/ProtectedRoutesComponent";
+// import Certificate from "../src/components/certificateGenretor/Certificate";
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<Website />} />
+            <Route path="*" element={<Navigate to="/" />} />
+            <Route exact path='/explore/company/:id' element={<CompanyPage />}></Route>
+            <Route exact path='/roles-jobdescriptions' element={<Jobs />}></Route>
+            <Route
+              element={<ProtectedRoutesComponent />}
+            >
+              <Route exact path='/home' element={<Home />}></Route>
+              <Route exact path='/dashboard' element={<Dashboard />}></Route>
+
+              <Route exact path='/account' element={<Profile />}></Route>
+              <Route exact path='/terms' element={<TermsAndConditions />}></Route>            
+              <Route exact path='/company/:id' element={<CompanyPage />}></Route>              
+              <Route exact path='/internship/:id' element={<InternshipPage />}></Route>
+              {/* <Route exact path='/certificate' element={<Certificate />}></Route> */}
+
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </AuthProvider>
+  );
+};
+
+export default App;
