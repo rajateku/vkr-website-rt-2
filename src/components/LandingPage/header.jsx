3ab827559239e80176a6c347faf2b5323ab4446d
vkrtech70 <vkrtech70@gmail.com>
2025-01-18 10:10:10 +0100

    Update header.jsx

--- src/components/LandingPage/header.jsx
+++ src/components/LandingPage/header.jsx
@@ -12,15 +12,16 @@ export const Header = (props) => {
                 <h1>
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
+                  
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                {/* <Link to="/signup" className='hide-text'>
+                <Link to="https://skills.vkrfoundry.com/" className='hide-text'>
                   <span className="btn-custom rounded-pill page-scroll">
-                    Sign up
+                    Skills and Internships
                   </span>
                 </Link>
-                {" "}
-                <Link to="/signin" className='hide-text'>
+                {/* {" "} */}
+                {/* <Link to="/signin" className='hide-text'>
                   <span className="btn-custom rounded-pill page-scroll" >
                      Sign in
                   </span>
